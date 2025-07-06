b23d2e7109f7d9c06f68c4ffef8b817756978b8b
cjmDevelop <jr87.dev@gmail.com>
2025-07-06 13:32:16 -0400

    changing to textContent to fix the innerText not formatting properly.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 // increment the count variable when the button is clicked
 //change the count-el in the HTML to refine the new count
 
-
 let count = 0;
 let newCount = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
     count++;
@@ -13,3 +13,8 @@ function increment() {
     console.log("Button was clicked!");
 }
 
+function save() {
+    let countDashula = count + " - ";
+    saveEl.textContent += countDashula;
+    console.log(count);
+}
