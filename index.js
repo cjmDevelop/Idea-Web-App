c7b61700acdac4b0d93872f699388f96661b0c71
cjmDevelop <jr87.dev@gmail.com>
2025-07-06 13:39:46 -0400

    Actually now fixed innerText to textContent and changed variable names to match scrimba course.

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 //change the count-el in the HTML to refine the new count
 
 let count = 0;
-let newCount = document.getElementById("count-el");
+let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 
 function increment() {
     count++;
-    newCount.innerText = count;
+    countEl.textContent = count;
     console.log("Button was clicked!");
 }
 
@@ -17,4 +17,6 @@ function save() {
     let countDashula = count + " - ";
     saveEl.textContent += countDashula;
     console.log(count);
+    count = 0;
+    countEl.textContent = count;
 }
