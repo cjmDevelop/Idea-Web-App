c3c3566e57fad17d241091adb785f643cfea6ad3
cjmDevelop <jr87.dev@gmail.com>
2025-07-06 08:34:01 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+//change the count-el in the HTML to refine the new count
+
+
+let count = 0;
+
+function increment() {
+    count++;
+
+    console.log("Button was clicked!");
+}
+
