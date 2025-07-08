3c5e11ed54c9878ede9512fc28c8e86570c95074
cjmDevelop <jr87.dev@gmail.com>
2025-07-08 14:01:33 -0400

    Updating the style and layout.

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// initialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-//change the count-el in the HTML to refine the new count
-
 let count = 0;
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
@@ -20,3 +15,4 @@ function save() {
     count = 0;
     countEl.textContent = count;
 }
+
