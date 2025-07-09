a3b5b73474c9de1541859cd845240d3bff7f48ce
cjmDevelop <63468241+cjmDevelop@users.noreply.github.com>
2025-07-09 14:30:28 -0400

    Site now will populate lightbulb image/icon representing an entered/saved idea.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,19 @@ let count = 0;
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("save-el");
 
+
 function increment() {
     count++;
     countEl.textContent = count;
     console.log("Button was clicked!");
+
+    let lightBulb = document.createElement("img");
+        lightBulb.src = "public/lightBulb-Icon.png";
+        lightBulb.alt = "Light bulb image representing entered & saved idea.";
+        lightBulb.style.width = "30px";
+
+    let brightIdea = document.getElementById("bright-ideas");
+        brightIdea.appendChild(lightBulb);
 }
 
 function save() {
@@ -15,4 +24,5 @@ function save() {
     count = 0;
     countEl.textContent = count;
 }
+
 
