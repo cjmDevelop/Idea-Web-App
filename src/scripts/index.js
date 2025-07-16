3ab7a19e34cd6e61ded41e573dc277695723f2dc
cjmDevelop <63468241+cjmDevelop@users.noreply.github.com>
2025-07-16 16:53:11 -0400

    Finally saving text to links, can see it in console.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,27 +1,32 @@
 const entries = [];
-const ideasEntered = document.getElementById("ideas-entered"); // idea counter, artifact from scrimba.com learn javascript for beginners, counter app lessons.
 const textAreaContent = document.getElementById("idea");// Text from textarea
 const ideasAsLights = document.getElementById("ideas-as-lights");// Visual images of lights as links for saved text from textarea
-
 let count = 0;
 
 function increment() {
-    count++;
-    /*Artifacts from scrimba.com while completing beginner's JavaScript app-counter lesson.
-    ideasEntered.textContent = count;
-    console.log("Button was clicked!");*/
 
-    const lightBulb = document.createElement("img");
-          lightBulb.src = "/lightBulb-Icon.png";
-          lightBulb.alt = "Light bulb image representing entered & saved idea.";
-          lightBulb.style.width = "30px";
-          lightBulb.style.margin = "5px";
-      
-    ideasAsLights.appendChild(lightBulb);
-    entries.push(textAreaContent.value);
-    textAreaContent.value = ""; // Visually refreshes the textarea
+    entries.push(textAreaContent.value);//Add entry to array
     
-    console.log(entries);// Logging for testing
+    const lightBulb = document.createElement("img");
+    lightBulb.src = "/lightBulb-Icon.png";
+    lightBulb.alt = "Light bulb image representing entered & saved idea.";
+    lightBulb.style.width = "30px";
+    lightBulb.style.margin = "5px";
+    lightBulb.style.cursor = "pointer";
+    
+    const anchorIdea = document.createElement("a");
+          anchorIdea.href = "#";
+          anchorIdea.appendChild(lightBulb);//Wraps image in anchor
+          anchorIdea.dataset.text = entries[count]; //Using dataset.text to "get" the text 
+          anchorIdea.addEventListener("click", (e) => {
+            e.preventDefault();
+            textAreaContent.value = e.currentTarget.dataset.text;
+            console.log(textAreaContent.value);
+          })
+   
+    ideasAsLights.appendChild(anchorIdea);//Appends anchor to HTML
+    textAreaContent.value = ""; //Visually refreshes the textarea
+    count++; // Increases count
 }
 
 
